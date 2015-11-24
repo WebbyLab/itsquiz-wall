1c4ef7e81e8ea158b402d1b2dc708d06c9b3a113
krambertech <k.porshnieva@gmail.com>
2015-11-24 20:54:15 +0200

    Move favicon to static

--- server/app.js
+++ server/app.js
@@ -89,6 +89,7 @@ function renderHTML({componentHTML, initialState, OGData, config}) {
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="shortcut icon" href="/static/favicon.ico"/>
             <title>Quiz Wall</title>
 
             <meta property="og:title" content="${OGData.title}" />
