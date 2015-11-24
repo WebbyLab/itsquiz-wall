17d75c029c289757d78bef633e8c344a6caafd71
krambertech <k.porshnieva@gmail.com>
2015-11-24 21:00:17 +0200

    Make locale parser case insenetive

--- shared/utils/index.js
+++ shared/utils/index.js
@@ -3,10 +3,11 @@
 import Jed from 'jed';
 
 export function extractSupportedLocaleFromPathname(path) {
+    const searchPath = path.toLowerCase();
     const SUPPORTED_LOCALES = ['ru', 'en', 'uk'];
     const DEFAULT_LOCALE = 'ru';
 
-    return SUPPORTED_LOCALES.find(locale => path.indexOf(`/${locale}/`) !== -1) || DEFAULT_LOCALE;
+    return SUPPORTED_LOCALES.find(locale => searchPath.indexOf(`/${locale}/`) !== -1) || DEFAULT_LOCALE;
 }
 
 export function sprintf(text, ...params) {
