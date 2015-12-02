c62ddc72b8cff3d4f4d5c3a47f3f765dc40d823f
krambertech <k.porshnieva@gmail.com>
2015-12-02 16:32:01 +0200

    Add google analytics code

--- server/app.js
+++ server/app.js
@@ -115,6 +115,13 @@ function renderHTML({componentHTML, initialState, metaData, config}) {
             <link rel="stylesheet" href="//cdn.materialdesignicons.com/1.2.65/css/materialdesignicons.min.css">
             <link rel="stylesheet" href="${config.staticUrl}/static/build/main.css">
             <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            </script>
         </head>
         <body>
         <div id="react-view">${componentHTML}</div>
