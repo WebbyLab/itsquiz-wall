b3348e8b38bb4810ab80a349e490ed0472b982a5
krambertech <k.porshnieva@gmail.com>
2015-12-02 16:53:38 +0200

    Make cards smaller

--- shared/components/pages/ActivationsPage.jsx
+++ shared/components/pages/ActivationsPage.jsx
@@ -56,7 +56,7 @@ export default class ActivationsPage extends React.Component {
                     <Cell
                         key    = {activation.id}
                         align  = 'stretch'
-                        col    = {4}
+                        col    = {3}
                         tablet = {6}
                         phone  = {12}>
                         <QuizCard
