8d1486f057fa832045b6e510b6aaccec128c2796
krambertech <k.porshnieva@gmail.com>
2015-12-02 16:32:45 +0200

    Add events where needed

--- shared/containers/App.jsx
+++ shared/containers/App.jsx
@@ -1,10 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 
+import { initialize, navigate } from '../utils/googleAnalytics';
+
 if ( process.env.BROWSER ) {
     require('../assets');
 }
 
 export default class App extends Component {
+    componentDidMount() {
+        initialize();
+        navigate({
+            page  : this.props.location.pathname,
+            title : this.props.routes[this.props.routes.length - 1].path
+        });
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.location.pathname !== nextProps.location.pathname) {
+            navigate({
+                page  : nextProps.location.pathname,
+                title : nextProps.routes[nextProps.routes.length - 1].path
+            });
+        }
+    }
+
     render() {
         return (
             <div id="app-view">
