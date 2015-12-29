4227ca1af9de1e9cb223ace4d9591356d9cb0fec
krambertech <k.porshnieva@gmail.com>
2015-12-29 11:38:04 +0200

    Use userId param to get autor actications

--- shared/actions/activations.js
+++ shared/actions/activations.js
@@ -35,23 +35,22 @@ export const LOAD_ACTIVATION_SUCCESS = 'LOAD_ACTIVATION_SUCCESS';
 export const LOAD_ACTIVATION_FAIL    = 'LOAD_ACTIVATION_FAIL';
 
 export function loadActivation({ id }, query = {}) {
-    return (dispatch) => {
+    const assigneeId = query.assigneeId || '';
+
+    return dispatch => {
         dispatch({ type : LOAD_ACTIVATION_REQUEST, activationId : id });
 
-        return api.activations.show(id, {
-            assigneeId : query.assigneeId || ''
-        }).then( (response) => {
-            const authorId = response.data.links.owner.id;
+        return api.activations.show(id, { assigneeId, include: 'users' }).then( response => {
+            const userId = response.data.links.owner.id;
 
-            return api.users.show(authorId, {include: 'activations'}).then( (response2) => {
+            return api.activations.list({ userId, assigneeId }).then( response2 => {
                 dispatch({
                     type              : LOAD_ACTIVATION_SUCCESS,
                     activation        : response.data,
-                    author            : response2.data,
-                    authorActivations : response2.linked.activations
+                    author            : response.linked.users.find(user => user.id === userId),
+                    authorActivations : response2.data.entities
                 });
             });
-
         }).catch( error => {
             dispatch({
                 type: LOAD_ACTIVATION_FAIL,
