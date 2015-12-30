db7c2d28b18f883cd3783943dd69f5f9a9a34e0f
krambertech <k.porshnieva@gmail.com>
2015-12-30 16:54:10 +0200

    Add route for results sharing

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -8,10 +8,11 @@ import App from './containers/App.jsx';
 import MainLayout from './containers/layouts/MainLayout.jsx';
 
 import ActivationsPageContainer from './containers/pages/ActivationsPage.jsx';
-import ActivationPageContainer from './containers/pages/ActivationPage.jsx';
+import ActivationPageContainer  from './containers/pages/ActivationPage.jsx';
+import ShareResultPageContainer from './containers/pages/ShareResultPage.jsx';
 
 import UsersPageContainer from './containers/pages/UsersPage.jsx';
-import UserPageContainer from './containers/pages/UserPage.jsx';
+import UserPageContainer  from './containers/pages/UserPage.jsx';
 
 export default (
     <Route component={App} >
@@ -20,6 +21,8 @@ export default (
 
             <Route component={ActivationsPageContainer} path='/activations' />
             <Route component={ActivationPageContainer} path='/activations/:id' />
+
+            <Route component={ShareResultPageContainer} path='/result/:id/:userId' />
 
             <Route component={UsersPageContainer} path='/users' />
             <Route component={UserPageContainer} path='/users/:id' />
