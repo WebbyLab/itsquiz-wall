d90230f67285c474b5eabe0fad93e7160bb312c4
artemT82 <ar.tymchenko@gmail.com>
2017-02-09 15:15:09 +0200

    Change code of Yandex Metrika to view statistics from different account

--- server/app.js
+++ server/app.js
@@ -197,35 +197,33 @@ function renderHTML({ componentHTML, initialState, metaData, config }) {
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <!-- Yandex.Metrika counter -->
             <script type="text/javascript">
-               (function (d, w, c) {
-                   (w[c] = w[c] || []).push(function() {
-                       try {
-                           w.yaCounter35666924 = new Ya.Metrika({
-                               id:35666924,
-                               clickmap:true,
-                               trackLinks:true,
-                               accurateTrackBounce:true
-                           });
-                       } catch(e) { }
-                   });
+                (function (d, w, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        try {
+                            w.yaCounter42694814 = new Ya.Metrika({
+                                id:42694814,
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true,
+                                webvisor:true,
+                                trackHash:true
+                            });
+                        } catch(e) { }
+                    });
 
-                   var n = d.getElementsByTagName("script")[0],
-                       s = d.createElement("script"),
-                       f = function () { n.parentNode.insertBefore(s, n); };
-                   s.type = "text/javascript";
-                   s.async = true;
-                   s.src = "https://mc.yandex.ru/metrika/watch.js";
+                    var n = d.getElementsByTagName("script")[0],
+                        s = d.createElement("script"),
+                        f = function () { n.parentNode.insertBefore(s, n); };
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = "https://mc.yandex.ru/metrika/watch.js";
 
-                   if (w.opera == "[object Opera]") {
-                       d.addEventListener("DOMContentLoaded", f, false);
-                   } else { f(); }
-               })(document, window, "yandex_metrika_callbacks");
+                    if (w.opera == "[object Opera]") {
+                        d.addEventListener("DOMContentLoaded", f, false);
+                    } else { f(); }
+                })(document, window, "yandex_metrika_callbacks");
             </script>
-            <noscript>
-                <div>
-                    <img src="https://mc.yandex.ru/watch/35666924" style="position:absolute; left:-9999px;" alt="" />
-                </div>
-            </noscript>
+            <noscript><div><img src="https://mc.yandex.ru/watch/42694814" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
             <!-- /Yandex.Metrika counter -->
         </head>
         <body>
