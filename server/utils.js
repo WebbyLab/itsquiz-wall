323d530c89da0da249501c500d7507a09366d722
artemT82 <ar.tymchenko@gmail.com>
2017-02-17 16:48:57 +0200

    Remove logs

--- server/utils.js
+++ server/utils.js
@@ -116,8 +116,6 @@ export function getIp(req) {
 export function detectLocale(req) {
     // Take locale passed by account
     const passedLocale = (req.query.locale || '').toLowerCase();
-    console.log('req.query.locale', req.query.locale);
-    console.log('req.cookies.locale', req.cookies.locale);
 
     if (getSupportedLocales().indexOf(passedLocale) >= 0) {
         return passedLocale;
@@ -127,9 +125,6 @@ export function detectLocale(req) {
     const ip = getIp(req);
     const geo = geoip.lookup(ip);
     const country = (geo && geo.country);
-
-    console.log('ip', ip);
-    console.log('country', country);
 
     return {
         UA: 'uk',
