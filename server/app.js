2d1de473810546463f7def5322a7885e29230715
Viktor Turskyi <koorchik@gmail.com>
2021-04-22 08:59:45 +0300

    Update app.js
    Remove Yandex Metrika

--- server/app.js
+++ server/app.js
@@ -195,36 +195,6 @@ function renderHTML({ componentHTML, initialState, metaData, config }) {
                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             </script>
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- Yandex.Metrika counter -->
-            <script type="text/javascript">
-                (function (d, w, c) {
-                    (w[c] = w[c] || []).push(function() {
-                        try {
-                            w.yaCounter42694814 = new Ya.Metrika({
-                                id:42694814,
-                                clickmap:true,
-                                trackLinks:true,
-                                accurateTrackBounce:true,
-                                webvisor:true,
-                                trackHash:true
-                            });
-                        } catch(e) { }
-                    });
-
-                    var n = d.getElementsByTagName("script")[0],
-                        s = d.createElement("script"),
-                        f = function () { n.parentNode.insertBefore(s, n); };
-                    s.type = "text/javascript";
-                    s.async = true;
-                    s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-                    if (w.opera == "[object Opera]") {
-                        d.addEventListener("DOMContentLoaded", f, false);
-                    } else { f(); }
-                })(document, window, "yandex_metrika_callbacks");
-            </script>
-            <noscript><div><img src="https://mc.yandex.ru/watch/42694814" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-            <!-- /Yandex.Metrika counter -->
         </head>
         <body>
         <div id="react-view">${componentHTML}</div>
